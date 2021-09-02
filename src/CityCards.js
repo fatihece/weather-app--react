516658b9654e0dc3a5fff6db3b36e9dfe90eb3a4
fatih <fatihenaz@gmail.com>
2021-09-02 10:49:21 +0300

    created components

---
+++ src/CityCards.js
@@ -0,0 +1,12 @@
+import React, {useEffect, useState} from 'react'
+import "./DefaultCards.css"
+
+const CityCards = () => {
+    return (
+        <div className="card-container">
+            
+        </div>
+    )
+}
+
+export default CityCards
