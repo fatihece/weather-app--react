a984ee58c9dfe7e126f6e46c730a6a96cbd2a85a
fatih <fatihenaz@gmail.com>
2021-09-03 12:33:27 +0300

    City cards updated

--- src/CityCards.js
+++ src/CityCards.js
@@ -2,12 +2,14 @@ import React from 'react'
 import "./CityCards.css"
 import Istanbul from "./CityCards/Istanbul"
 import London from './CityCards/London'
+import Sydney from "./CityCards/Sydney"
 
 const CityCards = () => {
     return (
         <div className="card-container">
             <Istanbul />
             <London />
+            <Sydney />
         </div>
     )
 }
